e5b5c53ce4d361ec036b23ae8dd78bfc79c716e4
jayamewada099-prog <jayamewada099@gmail.com>
2026-02-28 12:49:14 +0530

    changed image size to 256

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if uploaded_file:
             progress.progress(i + 1)
 
         # -------- IMAGE PREPROCESSING --------
-        img = image.resize((224, 224))
+        img = image.resize((256, 256))
         img_array = np.array(img) / 255.0
         img_array = np.expand_dims(img_array, axis=0)
 
